a9505e1bcda10c6ccc63f8d3f17afc2d8d04004e
Jimmy Bogard <jimmy.bogard@gmail.com>
2020-06-29 09:38:50 -0500

    Simplifying references

--- Push.ps1
+++ Push.ps1
@@ -1,7 +1,7 @@
 $scriptName = $MyInvocation.MyCommand.Name
 $artifacts = "./artifacts"
 
-if ($Env:NUGET_API_KEY -eq $null) {
+if ([string]::IsNullOrEmpty($Env:NUGET_API_KEY)) {
     Write-Host "${scriptName}: NUGET_API_KEY is empty or not set. Skipped pushing package(s)."
 } else {
     Get-ChildItem $artifacts -Filter "*.nupkg" | ForEach-Object {
